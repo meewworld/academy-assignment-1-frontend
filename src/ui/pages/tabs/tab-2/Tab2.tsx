0849f5c618f4af1cd2d519abdb6aa6f4fa678eba
Markido <markido@live.dk>
2023-01-20 09:55:32 +0100

    Revert "Merge pull request #5 from MeeWbrh/desktopBranch"
    This reverts commit 5062315cbf4328f9e284c12d9fca6216eb9d4b4c, reversing
    changes made to c038330d0d5792eb0061d2bd7d6733b6cfa13b80.

--- src/ui/pages/tabs/tab-2/Tab2.tsx
+++ src/ui/pages/tabs/tab-2/Tab2.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { IonContent, IonTitle } from '@ionic/react';
-import Tab2Form from 'ui/components/tabs/Tab2Form';
 
 const Tab2: React.FC = () => (
-  <IonContent>
-    <IonTitle>Change bg</IonTitle>
-    <Tab2Form></Tab2Form>
+  <IonContent color={'white-background'}>
+    <IonTitle>Tab2</IonTitle>
   </IonContent>
 );
 
