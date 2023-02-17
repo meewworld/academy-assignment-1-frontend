e07a21c31e989cd9fdcf28f32b7585c90fa31fb2
patr0335 <patr0335@stud.kea.dk>
2023-02-17 13:16:55 +0100

    tab 2 and 3 file changes

--- src/ui/pages/tabs/tab-2/Tab2.tsx
+++ src/ui/pages/tabs/tab-2/Tab2.tsx
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './tab-2.module.css';
-import { IonContent, IonList } from '@ionic/react';
+import { IonContent, IonList, IonSearchbar } from '@ionic/react';
 import GameCard from 'ui/components/ui-library/game-card/GameCard';
 
 const Tab2: React.FC = () => {
+  const [searchText, setSearchText] = useState('');
   const cardStyle: React.CSSProperties = {
     marginTop: '1.25rem',
   };
+  const games = [
+    { title: 'Game 1', gameName: 'game1', backgroundImg: 'dice1.jpg' },
+    { title: 'Game 2', gameName: 'game2', backgroundImg: 'dice2.jpg' },
+    { title: 'Game 3', gameName: 'game3', backgroundImg: 'sekz.jpeg' },
+    { title: 'Game 4', gameName: 'game4', color: '#06b867' },
+    { title: 'Game 5', gameName: 'game5', color: '#9ab806' },
+    { title: 'Game 6', gameName: 'game6', color: '#c90a3d' },
+  ];
   return (
     <IonContent>
+      <IonSearchbar
+        showClearButton="always"
+        animated={true}
+        value={searchText}
+        onIonChange={(e) => setSearchText(e.detail.value!)}
+        placeholder="Search games"
+      />
       <div className="mx-5 mt-5 flex flex-col">
         <IonList>
-          <div style={cardStyle}>
-            <GameCard title={'shieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet'} gameName={'shiet'} backgroundImg={'dice1.jpg'} />
-          </div>
-          <div style={cardStyle}>
-            <GameCard title={'shieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet'} gameName={'shiet'} backgroundImg={'dice2.jpg'} />
-          </div>
-          <div style={cardStyle}>
-            <GameCard title={'shieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet'} gameName={'shiet'} backgroundImg={'sekz.jpeg'} />
-          </div>
-          <div style={cardStyle}>
-            <GameCard title={'shieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet'} gameName={'shiet'} color='#06b867'/>
-          </div>
-          <div style={cardStyle}>
-            <GameCard title={'shieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet'} gameName={'shiet'} color='#9ab806'/>
-          </div>
-          <div style={cardStyle}>
-            <GameCard title={'shieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet'} gameName={'shiet'} color='#c90a3d'/>
-          </div>
+          {games
+            .filter((game) => game.title.toLowerCase().includes(searchText.toLowerCase()) || game.gameName.toLowerCase().includes(searchText.toLowerCase()))
+            .map((game) => (
+              <div style={cardStyle} key={game.gameName}>
+                <GameCard {...game} />
+              </div>
+            ))}
         </IonList>
       </div>
     </IonContent>
--- src/ui/pages/tabs/tab-3/Tab3.tsx
+++ src/ui/pages/tabs/tab-3/Tab3.tsx
@@ -1,59 +1,20 @@
-import React, { useState, useRef } from 'react';
-import { IonButtons, IonButton, IonModal, IonHeader, IonContent, IonToolbar, IonTitle, IonPage, IonItem, IonLabel, IonInput } from '@ionic/react';
-import { OverlayEventDetail } from '@ionic/core/components';
+import React from 'react';
+import { IonContent, IonTitle } from '@ionic/react';
+import TakePicture from 'ui/components/frontpage/take-picture/TakePicture';
+import { Photo } from '@capacitor/camera';
 
-const Example: React.FC = () => {
-  const modal = useRef<HTMLIonModalElement>(null);
-  const input = useRef<HTMLIonInputElement>(null);
+const Tab3: React.FC = () => {
 
-  const [message, setMessage] = useState('This modal example uses triggers to automatically open a modal when the button is clicked.');
-
-  function confirm() {
-    modal.current?.dismiss(input.current?.value, 'confirm');
-  }
-
-  function onWillDismiss(ev: CustomEvent<OverlayEventDetail>) {
-    if (ev.detail.role === 'confirm') {
-      setMessage(`Hello, ${ev.detail.data}!`);
-    }
-  }
+  const [picture, setPicture] = React.useState<Photo>();
 
   return (
-    <IonContent>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Inline Modal</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <IonButton id="open-modal" expand="block">
-          Open
-        </IonButton>
-        <p>{message}</p>
-        <IonModal ref={modal} trigger="open-modal" onWillDismiss={(ev) => onWillDismiss(ev)}>
-          <IonHeader>
-            <IonToolbar>
-              <IonButtons slot="start">
-                <IonButton onClick={() => modal.current?.dismiss()}>Cancel</IonButton>
-              </IonButtons>
-              <IonTitle>Welcome</IonTitle>
-              <IonButtons slot="end">
-                <IonButton strong={true} onClick={() => confirm()}>
-                  Confirm
-                </IonButton>
-              </IonButtons>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent className="ion-padding">
-            <IonItem>
-              <IonLabel position="stacked">Enter your name</IonLabel>
-              <IonInput ref={input} type="text" placeholder="Your name" />
-            </IonItem>
-          </IonContent>
-        </IonModal>
-      </IonContent>
+    <IonContent color={'white-background'}>
+      {!picture && <IonTitle>Take a photo!</IonTitle>}
+      {picture && <img src={picture?.webPath} alt="your upload" className="h-full w-auto m-auto" />}
+
+      <TakePicture onPictureTaken={(p) => setPicture(p)} />
     </IonContent>
   );
 };
 
-export default Example;
+export default Tab3;
